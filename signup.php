4daf9f02940c5c7b3fa0870765c1ae9857cf0f68
Taylor Becker <taylorjosephbecker@gmail.com>
2015-08-18 20:06:46 -0400

    Implemented PHPMailer using Composer, added configuration file(conf.ini)

--- signup.php
+++ signup.php
@@ -1,28 +1,64 @@
+
 <?php
-if (isset($_POST['submit']))
+require __DIR__ . '/vendor/autoload.php';
+
+if ( isset ( $_POST ['submit'] ) )
 {
-    $to = "someemail";
-    
+    $conf = parse_ini_file ( "conf.ini", true ); // mail config
+                                                 
     // form info
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $contactPref = $_POST['contactPref'];
-    $message = $_POST['message'];
-    $area = $_POST['area'];
+    $name = $_POST ['name'];
+    $email = $_POST ['email'];
+    $phone = $_POST ['phone'];
+    $contactPref = $_POST ['contactPref'];
+    $message = $_POST ['message'];
+    $area = $_POST ['area'];
+    $mapName = $_POST ['mapName'];
     
-    // create message
-    $subject = "[WavyLeaf Map Signup] " . $name . ": Area " . $area;
+    // send email
+    $signup = new PHPMailer (); // send mail to sign up
+                                
+    // $signup->SMTPDebug = 3; // Enable verbose debug output
     
-    $emailMessage = $name . " has claimed area " . $area . ".\n\nEmail: " . $email . "\nPhone: " . $phone . "\nPreference: " . $contactPref . "\n\n" . $name . " included the following message:\n----------------------------------------\n" . $message;
-    $emailMessage2 = "You have claimed area " . $area . ".\n\nEmail: " . $email . "\nPhone: " . $phone . "\nPreference: " . $contactPref . "\n\nYou included the following message:\n----------------------------------------\n" . $message;
+    $signup->isSMTP (); // Set mailer to use SMTP
+    $signup->Host = $conf ['MAIL'] ['host'];
+    $signup->SMTPAuth = true;
+    $signup->Username = $conf ['MAIL'] ['username'];
+    $signup->Password = $conf ['MAIL'] ['password'];
+    $signup->SMTPSecure = $conf ['MAIL'] ['secure'];
+    $signup->Port = $conf ['MAIL'] ['port'];
+    $signup->From = $conf ['MAIL'] ['username'];
+    $signup->FromName = $conf ['MAIL'] ['name'];
     
-    $headers = "From: " . $email . "\r\nReply-to: " . $email;
-    $headers2 = "From:" . $to;
+    // TODO Does this work?
+    $confirmation = clone $signup; // copy SMTP settings to mail to send to user who signed up
     
-    // send mail
-    mail($to,$subject,$emailMessage,$headers);
-    mail($email,$subject,$emailMessage2,$headers2);
-    echo "Mail Sent. Thank you for participating!";
+    $signup->addAddress ( $conf ['MAIL'] ['toEmail'], $conf ['MAIL'] ['name'] ); // send wavyleaf email
+    $signup->addReplyTo ( $email, $name );
+    
+    $signup->isHTML ( false ); // Set email format to HTML
+    
+    $signup->Subject = '[WavyLeaf Map Signup] ' . $name . ': Area ' . $area;
+    $signup->Body = $name . " has claimed area " . $area . ".\n\nEmail: " . $email . "\nPhone: " . $phone . "\nPreference: " . $contactPref . "\n\n" . $name . " included the following message:\n----------------------------------------\n" . $message;
+    
+    // send signup confirmation
+    $confirmation->addAddress ( $email );
+    $confirmation->addReplyTo ( $conf ['MAIL'] ['toEmail'], $conf ['MAIL'] ['name'] );
+    
+    $confirmation->isHTML ( true ); // Set email format to HTML
+    
+    $confirmation->Subject = '[WavyLeaf Map Signup] ' . $name . ': Area ' . $area;
+    $confirmation->Body = "Thank you for signing up for Wavyleaf Area " . $area . "!<br><br>Area Map<br><img style=\"width:200px;height:200px;\" src=\"" . $conf ['websiteHome'] . "inc/images/maps/area" . $mapName . ".png\" alt=\"Wavyleaf Area " . $area . "\"><br><a href=\"" . $conf ['websiteHome'] . "inc/images/maps/area" . $mapName . ".png\">PNG</a> | <a href=\"" . $conf ['websiteHome'] . "inc/pdf/area" . $mapName . ".pdf\">PDF</a>";
+    $confirmation->AltBody = "Thank you for signing up for Wavyleaf Area " . $area . "!\n\nYou can find maps of the area below:\n" . "PNG: " . $conf ['websiteHome'] . "inc/images/maps/area" . $mapName . ".png\nPDF: " . $conf ['websiteHome'] . "inc/pdf/area" . $mapName . ".pdf";
+    
+    if ( ! ($signup->send () && $confirmation->send ()) )
+    {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $signup->ErrorInfo;
+    }
+    else
+    {
+        echo "Mail Sent. Thank you for participating!";
+    }
 }
 ?>
